681b6c1f6992feda305099fb1ad7e367776b8445
sakshisrivastava126 <srivastavasakshi126@gmail.com>
2026-03-04 23:55:53 +0530

    added approveContent

---
+++ client/app/approve/approveContent.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+export default function ApproveContent() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  return <div>Token: {token}</div>;
+}
