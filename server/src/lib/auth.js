01bd45b7b10ac6f8dfc4aba407d72941e90b8834
sakshisrivastava126 <srivastavasakshi126@gmail.com>
2026-03-07 01:37:36 +0530

    changed trustedOrigin

--- server/src/lib/auth.js
+++ server/src/lib/auth.js
@@ -1,7 +1,7 @@
-import {betterAuth} from "better-auth";
-import {prismaAdapter} from "better-auth/adapters/prisma"
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma"
 import prisma from "./db.js"
-import { deviceAuthorization } from "better-auth/plugins"; 
+import { deviceAuthorization } from "better-auth/plugins";
 // import { PrismaClient } from "@prisma/client"
 
 // const prisma = new PrismaClient();
@@ -9,26 +9,32 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
-    basePath:"/api/auth",
-    trustedOrigins:["http://localhost:3000",
+    basePath: "/api/auth",
+    trustedOrigins: ["http://localhost:3000",
         "https://sebi-cli.vercel.app",
-    "https://sebicli-production.up.railway.app",
-    "https://sebicli-frontend-production.up.railway.app",
+        "https://sebicli-production.up.railway.app",
+        "https://sebicli-frontend-production.up.railway.app",
     ],
     plugins: [
         deviceAuthorization({
             verificationUri: "https://sebi-cli.vercel.app/sign-in",
             // clientId: "sebi-cli",        
-            expiresIn: "10m",              
+            expiresIn: "10m",
             validateClient: async (clientId) => {
                 return clientId === "sebi-cli";
             },
         }),
     ],
-    socialProviders:{
-        github:{
+    socialProviders: {
+        github: {
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET
+        }
+    },
+    advanced: {
+        defaultCookieAttributes: {
+            sameSite: "none",
+            secure: true,
         }
     }
 });
